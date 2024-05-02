ab47c6b1c21f7ecce38ec42ddc34d9a07e8208fe
Murshidmuhammedp <murshid.mmp@gmail.com>
2024-05-02 13:05:53 +0530

    update user

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,14 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get("/users", async (req,res)=>{
+
+router.get("/users", async (req, res) => {
   const datas = await User.find()
   res.status(200).send(datas)
 })
 
 router.post('/submit', async (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
 
   const { first_name, last_name, email, pw, pw_confirm } = req.body;
 
@@ -27,11 +28,25 @@ router.post('/submit', async (req, res) => {
     pw_confirm
   })
 
-  await newUser.save("")
+  await newUser.save()
 
-    res.status(201).send("User created Successfully")
+  res.status(201).send("User created Successfully")
 
 });
+
+router.put("/users/edit/:id", async (req, res) => {
+  const id = req.params.id
+  const userid = await User.findById(id);
+  const { first_name, last_name, email, pw, pw_confirm } = req.body;
+
+  if (first_name) userid.first_name = first_name
+  if (last_name) userid.last_name = last_name
+  if (email) userid.email = email
+  if (pw) userid.pw = pw
+
+  userid.save()
+  res.status(200).send("update successfully")
+})
 
 mongoose.connect('mongodb://localhost:27017/store')
   .then(() => console.log("DB connected"))
